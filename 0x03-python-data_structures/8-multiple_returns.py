f44cffce18485b4bdc760eb6da8dc91d43f49182
Yasser Rahmouni <rahmouniyasser77@gmail.com>
2023-11-05 06:12:12 -0800

    task 8

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     length = len(sentence)
-    if sentence[0]:
+    if sentence != "":
         first = sentence[0]
     else:
         first = None
