189c0f0ce99a492e33e5eebe1207b7b664bb8c88
Yasser Rahmouni <rahmouniyasser77@gmail.com>
2023-11-08 08:07:13 -0800

    task 10

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary is None:
+    if a_dictionary is None or a_dictionary == {}:
         return None
     new_list = list(a_dictionary.values())
     max_value = new_list[0]
