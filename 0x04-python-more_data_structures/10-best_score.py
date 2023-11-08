01e40744f8e92feb8f8a7419bf42058960b0fba8
Yasser Rahmouni <rahmouniyasser77@gmail.com>
2023-11-08 08:05:07 -0800

    task 10

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    if a_dictionary is None:
+        return None
+    new_list = list(a_dictionary.values())
+    max_value = new_list[0]
+    for val in new_list:
+        if val > max_value:
+            max_value = val
+    for key in a_dictionary:
+        if a_dictionary[key] == max_value:
+            return key
