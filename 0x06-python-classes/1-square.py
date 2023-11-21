2a5448e4572f7d80c7f392e19d25756b892e5bde
Yasser Rahmouni <rahmouniyasser77@gmail.com>
2023-11-21 03:33:26 -0800

    task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+""" make an empty class """
+
+
+class Square:
+    """ a class about a square """
+
+    def __init__(self, size):
+        """ initialize attributes
+
+        Args:
+            size: represents size of square
+        """
+
+        self.__size = size
+    pass
