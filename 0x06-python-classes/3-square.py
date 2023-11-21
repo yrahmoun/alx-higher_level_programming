a5a7487758c8c21b668741f52cda419f053f4996
Yasser Rahmouni <rahmouniyasser77@gmail.com>
2023-11-21 07:41:39 -0800

    task 3

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+""" make an empty class """
+
+
+class Square:
+    """ a class about a square """
+
+    def __init__(self, size=0):
+        """ initialize attributes
+
+        Args:
+            size: represents size of square
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+    def area(self):
+        """ finds the area of the square"""
+
+        return (self.__size ** 2)
