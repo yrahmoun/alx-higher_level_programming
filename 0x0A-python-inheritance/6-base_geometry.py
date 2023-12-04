8b8436204c1bfb8fe0e472d948ce812c967173e7
Yasser Rahmouni <rahmouniyasser77@gmail.com>
2023-12-04 06:52:23 -0800

    task 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" module for BaseGeometry class """
+
+
+class BaseGeometry:
+    """ properties of the classs """
+
+    def area(self):
+        """calculates the area"""
+        raise Exception("area() is not implemented")
