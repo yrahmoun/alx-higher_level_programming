f8cd9a10678ca2e1eba1a209cb0084fcdce52806
Yasser Rahmouni <rahmouniyasser77@gmail.com>
2023-12-05 03:19:06 -0800

    task 3

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" module for to_json_string """
+import json
+
+
+def to_json_string(my_obj):
+    """ returns the JSON representation of a string """
+    return json.dumps(my_obj)
