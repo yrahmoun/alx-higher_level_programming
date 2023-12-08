aaaa6ed901ca8702d51dcfb102e214ef15035327
Yasser Rahmouni <rahmouniyasser77@gmail.com>
2023-12-08 04:18:04 -0800

    update

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -15,8 +15,51 @@ class Base:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
 
+    @staticmethod
     def to_json_string(list_dictionaries):
         """returns the JSON string representation"""
         if list_dictionaries is None or list_dictionaries == []:
             return []
         return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """writes the JSON representation of list_objs to a file"""
+        filename = cls.__name__ + ".json"
+        with open(filename, "w") as f:
+            if list_objs is None:
+                f.wtite("[]")
+            else:
+                dict_list = [obj.to_dictionary() for obj in list_objs]
+                f.write(cls.to_json_string(dict_list))
+
+    @staticmethod
+    def from_json_string(json_string):
+        """returns the list of the JSON representation json_string"""
+        if json_string is None or json_string == []:
+            return []
+        return json.loads(json_string)
+
+    @classmethod
+    def create(cls, **dictionary):
+        """returns an instance with all attributes already set"""
+        if dictionary and dictionary != []:
+            if cls.__name__ == "Rectangle":
+                inst = cls(1,1)
+            else:
+                inst = cls(1)
+            inst.update(**dictionary)
+            return inst
+
+    @classmethod
+    def load_from_file(cls):
+        """ returns a list of instances from a JSON file """
+        filename = cls.__name__ + ".json"
+        try:
+            with open(filename, 'r') as f:
+                json_string = f.read()
+                obj_dicts = Base.from_json_string(json_string)
+                return [cls.create(**obj_dict) for obj_dict in obj_dicts]
+        except FileNotFoundError:
+            return []
+
