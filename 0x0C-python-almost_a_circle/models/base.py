c17fa0c367914e6fd2d02880919b3f2807a0a306
Yasser Rahmouni <rahmouniyasser77@gmail.com>
2023-12-11 08:30:03 -0800

    update

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -27,7 +27,7 @@ class Base:
     def save_to_file(cls, list_objs):
         """writes the JSON representation of list_objs to a file"""
         filename = cls.__name__ + ".json"
-        with open(filename, "w"i, , encoding="utf-8") as f:
+        with open(filename, "w", encoding="utf-8") as f:
             if list_objs is None:
                 f.wtite("[]")
             else:
