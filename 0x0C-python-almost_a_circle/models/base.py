e89fba748439d2e626d62e3dfb447b08d1768f8f
Yasser Rahmouni <rahmouniyasser77@gmail.com>
2023-12-11 08:12:56 -0800

    update

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -19,7 +19,7 @@ class Base:
     def to_json_string(list_dictionaries):
         """returns the JSON string representation"""
         if list_dictionaries is None or list_dictionaries == []:
-            return []
+            return "[]"
         return json.dumps(list_dictionaries)
 
     @classmethod
