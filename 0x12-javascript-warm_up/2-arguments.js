b9fbc6214493c60ae475a675d9780c264f1ceed6
Yasser Rahmouni <rahmouniyasser77@gmail.com>
2024-02-12 03:30:45 -0800

    task 2

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,7 +1,9 @@
 #!/usr/bin/node
 const num = process.argv.length;
-if (num > 2) {
+if (num === 3) {
   console.log('Argument found');
+} else if (num > 3) {
+  console.log('Arguments found');
 } else {
   console.log('No argument');
 }
