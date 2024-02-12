0c76e7e3b683c877a5a7c7c79da34f8436848025
Yasser Rahmouni <rahmouniyasser77@gmail.com>
2024-02-12 04:40:33 -0800

    task 10

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+function factorial (x) {
+  if (x === 0 || isNaN(x)) return 1;
+  else {
+    return x * factorial(x - 1);
+  }
+}
+
+console.log(factorial(parseInt(process.argv[2])));
