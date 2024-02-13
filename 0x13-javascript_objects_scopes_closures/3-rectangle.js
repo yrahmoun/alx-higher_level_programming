5bab9b6eb9c2b29b7fe62be27128df3a2100ace7
Yasser Rahmouni <rahmouniyasser77@gmail.com>
2024-02-13 05:56:56 -0800

    task 3

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    this.width = w;
+    this.height = h;
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      let row = '';
+      for (let j = 0; j < this.width; j++) {
+        row += 'X';
+      }
+      console.log(row);
+    }
+  }
+}
+
+module.exports = Rectangle;
